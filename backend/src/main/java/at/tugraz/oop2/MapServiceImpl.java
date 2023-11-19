5e4b532376bcd0b2a396eaec46a5fd9af2186317
hannes hoffmann <hannes.hoffmann@student.tugraz.at>
2023-11-19 19:25:55 +0100

    nvm fixed it but grpc if fucking ass

--- backend/src/main/java/at/tugraz/oop2/MapServiceImpl.java
+++ backend/src/main/java/at/tugraz/oop2/MapServiceImpl.java
@@ -1,4 +1,10 @@
 package at.tugraz.oop2;
 
-public class MapServiceImpl extends mapserviceGRPC{
+import mapserviceGRPC.mapserviceGrpc;
+import mapserviceGRPC.req_ID;
+
+public class MapServiceImpl extends mapserviceGrpc.mapserviceImplBase{
+    @Override
+    public void getObjID(mapserviceGRPC.req_ID request,
+                  io.grpc.stub.StreamObserver<mapserviceGRPC.MapObject> responseObserver){}
 }
